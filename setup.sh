9e6474ee1631cd1720f2f95ef9154284c6f5db35
Saravana Periyasamy <saravanakumar.periyasamy@gmail.com>
2024-07-26 15:20:17 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,61 +4,28 @@
 
 brew update 
 brew tap caskroom/cask
-brew tap buildpack/tap
-brew tap bazelbuild/tap
 
+brew install --cask google-chrome
+brew install --cask spectacle
+brew install --cask maccy
+brew install --cask docker
+brew install --cask slack
+brew install --cask skitch
+brew install --cask insomnia
+brew install --cask visual-studio-code
+brew install --cask warp
+brew install --cask wireshark
 
-brew cask install iterm2
-brew cask install java
-brew cask install atom
-brew cask install google-chrome
-brew cask install spectacle
-brew cask install intellij-idea
-brew cask install google-cloud-sdk
-brew cask install google-backup-and-sync
-brew cask install clipy
-brew cask install docker
-brew cask install minikube
-brew cask install slack
-brew cask install virtualbox
-brew cask install vagrant
-brew cask install vagrant-manager
-brew cask install sourcetree
-brew cask install skitch
-brew cask install insomnia
-brew cask install visual-studio-code
-brew cask install wireshark
-
-
-brew install pack
-brew install fish
 brew install git
-brew install hub
 brew install kubectl
 brew install kubectx
 brew install kustomize
-brew install ansible
-brew install go
-brew install dep
 brew install kubernetes-helm
 brew install wget
 brew install watch
 brew install jq
 brew install awscli
-brew install hugo
 brew install telnet
-brew install kafkacat
-#brew install qemu libvirt gcc ruby ruby-build rbenv libiconv #https://github.com/vagrant-libvirt/vagrant-libvirt/issues/818#issue-263657604
-#brew services start libvirt
-#vagrant plugin install vagrant-libvirt
-brew install vagrant-completion
 brew install vegeta
-brew install npm
-brew install etcd
-brew install sphinx-doc
+brew install nvm
 brew install grpcurl
-brew install bazelbuild/tap/bazel
-
-
-
-npm install -g grpcc
