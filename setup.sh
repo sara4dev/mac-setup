b301b65d16945eba3362cff3646b1bb1531ebf63
Saravana Periyasamy <saravanakumar.periyasamy@gmail.com>
2024-07-26 15:23:18 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew update 
 brew tap caskroom/cask
